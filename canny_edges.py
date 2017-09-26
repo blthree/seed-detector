5cd4af47e737a8fd46155495076d45719a8e517f
blthree <blthree89@gmail.com>
2017-09-26 19:50:37 -0400

    add jupyter notebook and test image

--- canny_edges.py
+++ canny_edges.py
@@ -9,14 +9,14 @@ from skimage.util import img_as_ubyte
 from skimage.color import rgb2gray
 from skimage import io
 
-image = io.imread("C:\\Users\\blthr\\PycharmProjects\\seed-detector\\img\\IMG_20170906_190232691_HDR.jpg")
-im = rgb2gray(image[:][1600:4600])
+image = io.imread("img/seed_plate/jpg")
+im = rgb2gray(image)
 
 # Compute the Canny filter for two values of sigma
-#edges1 = feature.canny(im, sigma=1, low_threshold=10, high_threshold=50)
-edges1 = feature.canny(im, sigma=2, low_threshold=0.1, high_threshold=0.4)
-#edges2 = feature.canny(im, sigma=3, low_threshold=10, high_threshold=50)
-edges2 = feature.canny(im, sigma=2, low_threshold=0.05, high_threshold=0.4)#, high_threshold=50)
+edges1 = feature.canny(im, sigma=1, low_threshold=10, high_threshold=50)
+#edges1 = feature.canny(im, sigma=2, low_threshold=0.1, high_threshold=0.4)
+edges2 = feature.canny(im, sigma=3, low_threshold=10, high_threshold=50)
+#edges2 = feature.canny(im, sigma=2, low_threshold=0.05, high_threshold=0.4)#, high_threshold=50)
 
 # display results
 fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3),
